a10aa169a17f8429ef682a5f9475dfe68aabcf99
hex7c0 <carniellifrancesco@gmail.com>
2014-08-06 12:16:09 +0200

    lodash library

---
+++ examples/lodash.js
@@ -0,0 +1,33 @@
+"use strict";
+/**
+ * @file lodash example
+ * @module arc4
+ * @package arc4
+ * @subpackage examples
+ * @version 0.0.1
+ * @author hex7c0 <hex7c0@gmail.com>
+ * @license GPLv3
+ */
+
+/*
+ * initialize module
+ */
+// import
+try {
+    var rc4 = require('../index.min.js'); // use require('arc4') instead
+} catch (MODULE_NOT_FOUND) {
+    console.error(MODULE_NOT_FOUND);
+    process.exit(1);
+}
+
+var a = 'pippo'; // key
+var b = 'ciao'; // data
+var cipher = rc4(a,true);
+
+var d = cipher.codeString(b); // encrypt
+
+var e = cipher.codeString(d); // decrypt
+
+console.log('original: ' + b);
+console.log('encrypt: ' + d);
+console.log('decrypt: ' + e);
